b2878fb1088501e8dc78f3e1d09dcf96a6ac1814
Diawai <waiyannyein12332190@gamil.com>
2020-05-30 09:33:38 -0600

    wrong alert

--- myjs.js
+++ myjs.js
@@ -3,4 +3,8 @@ function myfunction()
         	var x, text;
         	x=document.getElementById('number').value;
         	if(isNaN(x)|| x<1 || x>5)
-        }
+
+        	{
+        		text="sorry man it's over";
+        	}
+        	
